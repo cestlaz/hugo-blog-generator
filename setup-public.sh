c504965fefa804ee3e5bb9933cb863b252fc58f8
Mike Zamansky <zamansky@gmail.com>
2020-11-20 16:42:59 -0500

    aded setup script notes

---
+++ setup-public.sh
@@ -0,0 +1,5 @@
+echo "delete public"
+# https://github.com/cestlaz/cestlaz.github.io
+#echo git submodule add -b main git@github.com:cestlaz/cestlaz.github.io.git public
+echo git submodule init
+echo git submodule update
